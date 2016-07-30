43d01770141a7e226b8dcef9bfdea7f6049bcb65
rana1926 <rana.kelani@gmail.com>
2016-07-30 10:07:04 +0300

    Half done

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,31 @@
   *
   */
 var deepEquals = function(obj1, obj2){
+  var result=false;
+  var keys1=Object.keys(obj1);
+  var keys2=Object.keys(obj2);
+  var keys;
+  for (var i = 0; i < keys1.length; i++) {
+    if(keys1[i]!== keys2[i]){
+      return keys=false;
+    }
+    return keys=true;
+  }
+  if(keys){
+    for (var key in obj1) {
+      if(obj1[key]===obj2[key]){
+        result=true;
+      }
+      if(obj1[key]!==obj2[key]){
+        result=false;
+      }
+      if(typeof(obj1[key])==="object"){
+        result = deepEquals(obj1[key], obj2[key])
+      }
+    }
+  }
+  return result;
 };
+
+deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+deepEquals({a:1, b: {c:3}},{a:1, b: {x:3}}); // true
