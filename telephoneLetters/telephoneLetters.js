e32f11be07a505861eda4dbe35f5f08b552f27f6
rana1926 <rana.kelani@gmail.com>
2016-08-02 09:51:37 +0300

    not finished

--- telephoneLetters/telephoneLetters.js
+++ telephoneLetters/telephoneLetters.js
@@ -27,3 +27,47 @@
   *  Why not filter your results to only return words contained in that file?
   *
   */
+  function telephoneWords (phoneNumber){
+    var arr = phoneNumber.split('');
+    var letters=[['0'],['1'],['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']];
+    var wordLetters=[];
+    var result=[];
+    for (var i = 0; i < arr.length; i++) {
+      if(arr[i]==='0'){
+        wordLetters.push('0')
+      }
+      if(arr[i]==='1'){
+        wordLetters.push(letters[1])
+      }
+      if(arr[i]==='2'){
+        wordLetters.push(letters[2])
+      }
+      if(arr[i]==='3'){
+        wordLetters.push(letters[3])
+      }
+      if(arr[i]==='4'){
+        wordLetters.push(letters[4])
+      }
+      if(arr[i]==='5'){
+        wordLetters.push(letters[5])
+      }
+      if(arr[i]==='6'){
+        wordLetters.push(letters[6])
+      }
+      if(arr[i]==='7'){
+        wordLetters.push(letters[7])
+      }
+      if(arr[i]==='8'){
+        wordLetters.push(letters[8])
+      }
+      if(arr[i]==='9'){
+        wordLetters.push(letters[9])
+      }
+    }
+    for (var i = 0; i < wordLetters.length; i++) {
+        for (var j = 0; j < wordLetters[i].length; j++) {
+          wordLetters[i][j]
+        }
+      }
+    return wordLetters;
+  }
