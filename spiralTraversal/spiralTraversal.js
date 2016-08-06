39220f79252cee68c9f3abfdbdfbd81cf14b4244
rana1926 <rana.kelani@gmail.com>
2016-08-06 10:11:41 +0300

    unfinished

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -11,8 +11,50 @@
 
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
+function sorter(a,b) {
+	if(a> b) return -1;
+	if(a< b) return 1;   
+	return 0; 
+}	
+/*var spiralTraversal = function(matrix){
+	var result=[];
+	var lastrow=matrix[matrix.length-1].sort(sorter);
+	for (var i = 0; i < matrix.length; i++) {
+		for (var j = 0; j < matrix[i].length; j++) {
+			if(i===matrix.length-1){
+				result.push(lastrow[j]);
+			}
+			if(i>0 && i<matrix[i].length-1){
+				result.push()
+			}
+			result.push(matrix[i][j])
+		}
+	}
+	return result;
+};*/
 
 var spiralTraversal = function(matrix){
-
+	var result=[];
+	var lastrow=matrix[matrix.length-1].sort(sorter);
+	for (var i = 0; i < matrix[0].length; i++) {
+		result.push(matrix[0][i]);
+	}
+	for (var i = 1; i < matrix.length-1; i++) {
+		result.push(matrix[i][matrix[i].length-1]);
+	}
+	for (var i = 0; i < lastrow.length; i++) {
+		console.log(lastrow[i])
+		result.push(lastrow[i]);
+	}
+	for (var i = matrix.length-2; i > 1; i--) {
+		if(result[result.length-1]===lastrow[lastrow.length-1]){
+			result.push(matrix[i][0]);
+		}
+	}
+	for (var i = 1; i < matrix.length; i++) {
+		matrix[i]
+	}
+	console.log(lastrow)
+	return result;
 };
 
