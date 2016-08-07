bd1626f1b79511b97a023a5456b80ee21c4d6e84
rana1926 <rana.kelani@gmail.com>
2016-08-07 10:02:39 +0300

    done

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -7,7 +7,38 @@ Example:
 findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 
 */
-
-var findAll = function(arr){
-
+function sorter(a,b) {
+	if(a< b) return -1;
+	if(a> b) return 1;   
+	return 0; 
 }
+var findAll = function(arr){
+	var appearance =Math.floor(arr.length/3)
+	var result=arr;
+	var n=1;
+	for (var i = 0; i < arr.length; i++) {
+		
+	}
+	
+	return result;
+}
+/*var findAll = function(arr){
+	var appearance =Math.floor(arr.length/3)
+	var result=[];
+	var sorted=arr.sort(sorter);
+	var n=1;
+	for (var i = 0; i < sorted.length; i++) {
+		if(sorted[i]===sorted[i+1]){
+			n++;
+			console.log(appearance);
+		}
+		if(n>appearance){
+			result.push(sorted[i])
+			n=1;
+		}
+		if(sorted[i]!==sorted[i+1]){
+			n=1;
+		}
+	}
+	return result;
+}*/
