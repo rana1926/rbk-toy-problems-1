ffd7aa3f4da09b2c738cc560042f2fb0347fdce2
rana1926 <rana.kelani@gmail.com>
2016-08-09 10:21:15 +0300

    not solved

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -7,6 +7,11 @@
  *  
  *  Be systematic with your approach - make a clear plan before you start writing code.
  *
+ [[true,false,false,false,false],
+ [false,false,false,false,false],
+ [false,false,false,false,false],
+ [false,false,false,false,false],
+ [false,false,false,false,false]]
  *  Extra credit: Make your solution work for a grid of any size.
  *
  */
@@ -27,6 +32,46 @@ var makeBoard = function(n) {
   board.hasBeenVisited = function(i, j) {
     return !!this[i][j];
   }
+  board.goLeft = function (i,j) {
+    if(j===0){
+      this.togglePiece(i,n-1)
+    }
+    else{
+      this.togglePiece(i,j-1) 
+    }
+  }
+  board.goRight = function(i, j) {
+    if(j===n-1){
+      this.togglePiece(i,0)
+    }
+    else{
+      this.togglePiece(i,j+1)
+    }
+  }
+  board.goUp = function(i, j) {
+    if(i===0){
+      this.togglePiece(n-1, j);
+    }
+    else{
+      this.togglePiece(i-1,j)
+    }
+  }
+  board.goDown = function(i, j) {
+    if(i===n-1){
+      this.togglePiece(0, j)
+    }
+    else{
+      this.togglePiece(i+1,j)
+    }
+  }
+  board.robotPaths = function() {
+    this.togglePiece(0,0)
+    for (var i = 0; i < x.length; i++) {
+      for (var j = 0; j < x[i].length; j++) {
+        if()
+      }
+    }
+  }
   return board;
 };
 
@@ -34,7 +79,12 @@ var makeBoard = function(n) {
 //var myBoard = makeBoard(5);
 //The answer for a board size 5 is 8512 unique paths
 
-var robotPaths = function(/*you can pass any parameters you need*/) {
 
+var robotPaths = function() {
+  for (var i = 0; i < x.length; i++) {
+    for (var j = 0; j < x[i].length; j++) {
+      x[i][j]
+    }
+  }
 }
 
